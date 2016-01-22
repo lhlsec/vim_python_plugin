56f4edcb25148e590d7a09719767c3e3099142bd
lhlsec <lhlsec@gmail.com>
2016-01-23 00:09:23 +0800

    second commit

---
+++ vim.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# install fisa vim config
+
+echo '==============================='
+echo 'start to install dependences...'
+case "$OSTYPE" in
+    darwin*)  brew install vim git pip curl;;
+    linux*)   sudo apt-get install vim exuberant-ctags git pip curl;;
+    *)        echo "unknown: OS: $OSTYPE, U should install dependences by yourself" ;;
+esac
+sudo pip install dbgp vim-debug pep8 flake8 pyflakes isort
+
+echo '==============================='
+echo 'start to download vimrc file...'
+cp ~/.vimrc /tmp/vimrc.bak
+curl -O https://raw.githubusercontent.com/fisadev/fisa-vim-config/master/.vimrc
+mv .vimrc ~/.vimrc
+
+echo '==============================='
+echo 'start to install vim plugins...'
+vim +BundleClean +BundleInstall! +qa
+
+sudo chown -R $USER ~/.vim/  
